bc82edc2b57ff31f3a25478e53595245bb287254
Daryamals <119666576+Daryamals@users.noreply.github.com>
2025-05-13 14:33:09 +0300

    Create test_data.sql

---
+++ Task_Q4/test_data.sql
@@ -0,0 +1,10 @@
+INSERT INTO wp_users (ID, display_name, user_email, user_registered)
+VALUES
+(1, 'test1', 'alexey@example.com', '2024-01-15'),
+(2, 'test2', 'maria@example.com', '2024-03-10');
+
+INSERT INTO wp_posts (post_author, post_title, post_date)
+VALUES
+(1, 'Первая статья', '2024-04-01'),
+(2, 'Вторая статья', '2024-04-02'),
+(1, 'Третья статья', '2024-04-03');
