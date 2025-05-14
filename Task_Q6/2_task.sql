cd8281a36fa79635a581690e688af49c90dcd4a6
Daryamals <119666576+Daryamals@users.noreply.github.com>
2025-05-14 12:07:28 +0300

    Create 2_task.sql

---
+++ Task_Q6/2_task.sql
@@ -0,0 +1,5 @@
+INSERT INTO wp_users (display_name, user_email, user_registered)
+VALUES ('test1', 'test@example.com', NOW());
+
+INSERT INTO wp_posts (post_author, post_title, post_date)
+VALUES (1, '', NOW());
