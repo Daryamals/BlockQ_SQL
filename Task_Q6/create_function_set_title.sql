d21f65c34326ae3654b31819ac42c52ed8d96522
omanytea <omanytea@school21.ru>
2025-05-14 11:46:32 +0300

    delete

--- Task_Q6/create_function_set_title.sql
+++
@@ -1,9 +0,0 @@
-CREATE OR REPLACE FUNCTION set_default_title()
-RETURNS TRIGGER AS $$
-BEGIN
-    IF TRIM(NEW.post_title) IS NULL OR TRIM(NEW.post_title) = '' THEN
-        NEW.post_title := 'Anonymous';
-    END IF;
-    RETURN NEW;
-END;
-$$ LANGUAGE plpgsql;
